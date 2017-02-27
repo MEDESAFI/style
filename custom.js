cd1361831af4bd7624f8d7e83b596556d8a8d46c
MEDESAFI <one.avisez@gmail.com>
2017-02-27 22:47:01 +0000

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,24 @@
+$(function() { 
+
+    $("html").niceScroll({
+        cursorborderradius: "1px",
+        cursorborder: "none",
+        cursorwidth: "7",
+        cursorcolor: "#557799",
+        autohidemode: "none",
+        });
+});
+
+$(document).ready(function(){
+    $(".button-one").click(function(){
+        $('.one').fadeIn();
+    });
+    $(".closer").click(function(){
+        $('.one').fadeOut();
+    });
+    $('.one').click(function(evt){    
+            if(!$(evt.target).is('.win, .winh2, .winp')) {
+                    $('.one').fadeOut();
+             }
+            });
+});
